6a9932b62c34745b6852ed1c58d05930e095413e
Babalola Raphael Kehinde <kennybabs100@gmail.com>
2023-07-19 15:54:56 +0000

    new file

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,37 +1,38 @@
 #!/usr/bin/env python3
-"""
-Implements an expiring web cache and tracker
-"""
-from typing import Callable
-from functools import wraps
+'''A module with tools for request caching and tracking.
+'''
 import redis
 import requests
+from functools import wraps
+from typing import Callable
 
-redis_client = redis.Redis()
+
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
-def url_count(method: Callable) -> Callable:
-    """counts how many times an url is accessed"""
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
     @wraps(method)
-    def wrapper(*args, **kwargs):
-        url = args[0]
-        redis_client.incr(f"count:{url}")
-        cached = redis_client.get(url)  # Remove unnecessary f-string
-        if cached:
-            return cached.decode('utf-8')
-        response = method(url)  # Fetch the page content
-        redis_client.setex(url, 10, response)  # Fix setex arguments
-        return response
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
-    return wrapper
 
-
-@url_count
+@data_cacher
 def get_page(url: str) -> str:
-    """get a page and cache value"""
-    response = requests.get(url)
-    return response.text
-
-
-if __name__ == "__main__":
-    print(get_page('http://slowwly.robertomurray.co.uk'))
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
