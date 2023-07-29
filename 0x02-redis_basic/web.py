f257d0b75cfa1d751e48e42f3fb197be04fa2617
geeflows44 <57746361+geeflows44@users.noreply.github.com>
2023-07-29 21:27:53 +0100

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,36 +2,20 @@
 """
 Implements an expiring web cache and tracker
 """
-from typing import Callable
-from functools import wraps
 import redis
 import requests
-
-redis_client = redis.Redis()
-
-
-def url_count(method: Callable) -> Callable:
-    """counts how many times an url is accessed"""
-    @wraps(method)
-    def wrapper(*args, **kwargs):
-        url = args[0]
-        redis_client.incr(f"count:{url}")
-        cached = redis_client.get(url)  # Remove unnecessary f-string
-        if cached:
-            return cached.decode('utf-8')
-        response = method(url)  # Fetch the page content
-        redis_client.setex(url, 10, response)  # Fix setex arguments
-        return response
-
-    return wrapper
+rc = redis.Redis()
+count = 0
 
 
-@url_count
 def get_page(url: str) -> str:
-    """get a page and cache value"""
-    response = requests.get(url)
-    return response.text
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
+    return resp.text
 
 
 if __name__ == "__main__":
-    print(get_page('http://slowwly.robertomurray.co.uk'))
+    get_page('http://slowwly.robertomurray.co.uk')
